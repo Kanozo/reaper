12040a8d27c19da7e60987e71e39abd752bb3e8d
Kanozo <gkanozo@gmail.com>
2026-06-12 04:59:46 -0400

    fix typo error en facebook profile parser

--- src/reaper/parsers/facebook/profile_parser.py
+++ src/reaper/parsers/facebook/profile_parser.py
@@ -140,7 +140,7 @@ class ProfileParser(FacebookContentParser):
             self._extract_contact_info() 
             self._extract_timeline_feed()
             self._parse_traffic()
-            logger.indebugfo(
+            logger.debug(
                 "Perfil parseado. Feed: %d posts, Álbumes: %d.",
                 len(self.result["feed"])
             )
